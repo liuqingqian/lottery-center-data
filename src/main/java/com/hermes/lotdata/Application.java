1dc3f41b7a409ad0cf6a381b5ddfb45be508e410
liuqingqian <liu.qingqian@beicaizs.com>
2022-09-14 01:41:12 +0800

    去掉Redis配置

--- src/main/java/com/hermes/lotdata/Application.java
+++ src/main/java/com/hermes/lotdata/Application.java
@@ -5,6 +5,8 @@ import com.beicai.common.annotation.ExcludeComponent;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
@@ -12,7 +14,11 @@ import org.springframework.context.annotation.FilterType;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 
-@SpringBootApplication(exclude = DruidDataSourceAutoConfigure.class)
+@SpringBootApplication(exclude = {
+        DruidDataSourceAutoConfigure.class,
+        RedisAutoConfiguration.class,
+        RedisRepositoriesAutoConfiguration.class
+})
 @ServletComponentScan
 @EnableScheduling
 @EnableFeignClients(basePackageClasses = Application.class, basePackages = {"com.hermes.lotdata"})
